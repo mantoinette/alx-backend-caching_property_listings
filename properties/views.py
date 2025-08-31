012685f44b1269d85680a385b9f4ccd80aa5d219
mantoinette <152092675+mantoinette@users.noreply.github.com>
2025-08-31 15:40:09 +0200

    Update views.py

--- properties/views.py
+++ properties/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
 from .models import Property
+from django.http import JsonResponse", "return JsonResponse({"data"]
 
 @cache_page(60 * 15)  # cache for 15 minutes
 def property_list(request):
