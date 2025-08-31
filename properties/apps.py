df3bf7ae017ca2465919b74388ff6eeefb3b77b7
mantoinette <antoni.kigali@gmail.com>
2025-08-31 16:04:00 +0200

    Add signals to clear Redis cache on Property save/delete and configure app ready()

--- properties/apps.py
+++ properties/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+
+from django.apps import AppConfig
+
+class PropertiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'properties'
+
+def ready(self):
+        # Import signals so that they are registered
+        import properties.signals
